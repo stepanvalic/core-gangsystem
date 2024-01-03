d4fb00f284118a97a60f046aa48143f075be83b8
stepanvalic <131240792+stepanvalic@users.noreply.github.com>
2024-01-03 16:35:42 +0100

    Update config.lua

--- core-gangsystem/config.lua
+++ core-gangsystem/config.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.core = 'qb-core'                         --use your own server core like(qb-core,etc..)
+Config.core = 'qbx_core'                         --use your own server core like(qb-core,etc..)
 Config.cuffs = 'handcuffs'                        --this the item u need  to be able to open the actions menu
 Config.escortevent = 'police:client:EscortPlayer' --if youre using qb-policejob leave it like this else use ur own event
 Config.cuffevent = 'police:client:CuffPlayer'     --if youre using qb-policejob leave it like this else use ur own event
