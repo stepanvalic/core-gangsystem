6d3c96119c83f4ff0a237f5d24f614b3a3a77362
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-03 15:33:22 +0000

    Update config.lua

--- core-gangsystem/config.lua
+++ core-gangsystem/config.lua
@@ -6,31 +6,3 @@ Config.cuffevent = 'police:client:CuffPlayer'     --if youre using qb-policejob 
 Config.putplayerevent = 'police:client:PutPlayerInVehicle' --if youre using qb-policejob leave it like this else use ur own event
 Config.putplayeroutevent = 'police:client:SetPlayerOutVehicle' --if youre using qb-policejob leave it like this else use ur own event
 Config.robplayerevent = 'police:client:RobPlayer'              --if youre using qb-policejob leave it like this else use ur own event
-
-
-
---[[
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-
-    cccccccccccccccc        ooooooooooo        rrrrr   rrrrrrrrr            eeeeeeeeeeee
-  cc:::::::::::::::c      oo:::::::::::oo      r::::rrr:::::::::r         ee::::::::::::ee
- c:::::::::::::::::c     o:::::::::::::::o     r:::::::::::::::::r       e::::::eeeee:::::ee
-c:::::::cccccc:::::c     o:::::ooooo:::::o     rr::::::rrrrr::::::r     e::::::e     e:::::e
-c::::::c     ccccccc     o::::o     o::::o      r:::::r     r:::::r     e:::::::eeeee::::::e
-c:::::c                  o::::o     o::::o      r:::::r     rrrrrrr     e:::::::::::::::::e
-c:::::c                  o::::o     o::::o      r:::::r                 e::::::eeeeeeeeeee
-c::::::c     ccccccc     o::::o     o::::o      r:::::r                 e:::::::e
-c:::::::cccccc:::::c     o:::::ooooo:::::o      r:::::r                 e::::::::e
- c:::::::::::::::::c     o:::::::::::::::o      r:::::r                  e::::::::eeeeeeee
-  cc:::::::::::::::c      oo:::::::::::oo       r:::::r                   ee:::::::::::::e
-    cccccccccccccccc        ooooooooooo         rrrrrrr                     eeeeeeeeeeeeee
-
-
-
-]]
