98d0a509534ef537b00815b915c334ab6f839c71
Jay's Script's <116266395+Jays-Scripts@users.noreply.github.com>
2024-01-03 15:33:00 +0000

    cleaned up code

--- core-gangsystem/client/client.lua
+++ core-gangsystem/client/client.lua
@@ -1,30 +1,18 @@
 local QBCore = exports[Config.core]:GetCoreObject()
 
---[[
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
+-- FUNCTIONS --
+function _0xhas(item)
+    return QBCore.Functions.HasItem(item)
+end
 
-    cccccccccccccccc        ooooooooooo        rrrrr   rrrrrrrrr            eeeeeeeeeeee
-  cc:::::::::::::::c      oo:::::::::::oo      r::::rrr:::::::::r         ee::::::::::::ee
- c:::::::::::::::::c     o:::::::::::::::o     r:::::::::::::::::r       e::::::eeeee:::::ee
-c:::::::cccccc:::::c     o:::::ooooo:::::o     rr::::::rrrrr::::::r     e::::::e     e:::::e
-c::::::c     ccccccc     o::::o     o::::o      r:::::r     r:::::r     e:::::::eeeee::::::e
-c:::::c                  o::::o     o::::o      r:::::r     rrrrrrr     e:::::::::::::::::e
-c:::::c                  o::::o     o::::o      r:::::r                 e::::::eeeeeeeeeee
-c::::::c     ccccccc     o::::o     o::::o      r:::::r                 e:::::::e
-c:::::::cccccc:::::c     o:::::ooooo:::::o      r:::::r                 e::::::::e
- c:::::::::::::::::c     o:::::::::::::::o      r:::::r                  e::::::::eeeeeeee
-  cc:::::::::::::::c      oo:::::::::::oo       r:::::r                   ee:::::::::::::e
-    cccccccccccccccc        ooooooooooo         rrrrrrr                     eeeeeeeeeeeeee
+function InviteMembers()
+    local input = lib.inputDialog('Invite a member', { 'PlayerId' })
+    local playerData = QBCore.Functions.GetPlayerData()
+    if not input then return end
+    TriggerServerEvent('core-gangsystem:serversendinvite', input[1], playerData.gang.name)
+end
 
-
-
-]]
+-- EVENTS --
 RegisterNetEvent('core-gangsystem:client:notify', function(title, position, type)
     lib.notify({
         title = title,
@@ -74,13 +62,11 @@ RegisterNetEvent('core-gangsystem:client:adminmenu', function()
                 title = 'Gangs',
                 menu = 'admin_menu',
                 options = options
-
-
-
             })
         end
     end)
 end)
+
 RegisterNetEvent('core-gangsystem:managegang', function(gang)
     lib.registerContext({
         id = 'gangs_view1',
@@ -111,12 +97,11 @@ RegisterNetEvent('core-gangsystem:managegang', function(gang)
                 end,
                 icon = 'plus'
             },
-
         }
-
     })
     lib.showContext('gangs_view1')
 end)
+
 RegisterNetEvent('core-gangsystem:client:addgangpoints', function(gang)
     local input = lib.inputDialog('Add gangpoints to ' .. gang .. ' gang', {
         { type = 'number', label = 'Choose the number of points', icon = 'hashtag' },
@@ -124,14 +109,13 @@ RegisterNetEvent('core-gangsystem:client:addgangpoints', function(gang)
     if not input then return end
     TriggerServerEvent('core-gangsystem:addgangpoints', input[1], gang)
 end)
+
 function Gangcreateinput()
     local input = lib.inputDialog('Create Gang', { 'Gang Name', 'Gang Label', 'Gang leader(Player Id)' })
     if not input then return end
-
     local gangName = input[1]
     local gangLabel = input[2]
     local gangLeaderId = input[3]
-
     -- Check if any of the input fields are missing
     if not gangName or not gangLabel or not gangLeaderId or gangName == '' or gangLabel == '' or gangLeaderId == '' then
         lib.notify({
@@ -141,8 +125,7 @@ function Gangcreateinput()
         })
         return
     end
-
-    TriggerServerEvent('core-gangsystem:server:setgang', gangLeaderId, gangName, gangLabel)
+    TriggerServerEvent('core-gangsystem:server:setgang', tonumber(gangLeaderId), gangName, gangLabel)
 end
 
 RegisterNetEvent('core-gangsystem:client:gangmenu', function()
@@ -168,7 +151,6 @@ RegisterNetEvent('core-gangsystem:client:gangmenu', function()
                             end,
                             icon = 'user-plus'
                         },
-
                         {
                             title = 'Details',
                             description = 'Details About the gang',
@@ -177,7 +159,6 @@ RegisterNetEvent('core-gangsystem:client:gangmenu', function()
                                 { label = 'Members ',     value = gangcount },
                                 { label = 'Gang Name ',   value = QBCore.Functions.GetPlayerData().gang.name },
                                 { label = 'Gang grade ',  value = QBCore.Functions.GetPlayerData().gang.grade.name }
-
                             },
                             icon = 'info-circle'
                         },
@@ -202,12 +183,9 @@ RegisterNetEvent('core-gangsystem:client:gangmenu', function()
                                 { label = 'Gang Points ', value = gangpoints },
                                 { label = 'Gang Name ',   value = QBCore.Functions.GetPlayerData().gang.name },
                                 { label = 'Gang grade ',  value = QBCore.Functions.GetPlayerData().gang.grade.name }
-
                             },
                             icon = 'info-circle'
                         },
-
-
                     }
                 })
                 local options1 = {}
@@ -252,8 +230,6 @@ RegisterNetEvent('core-gangsystem:client:gangmenu', function()
                     id = 'actions_menu',
                     title = QBCore.Functions.GetPlayerData().gang.name,
                     options = options1
-
-
                 })
             end
             local gangmembers = {}
@@ -277,15 +253,10 @@ RegisterNetEvent('core-gangsystem:client:gangmenu', function()
                     options = gangmembers
                 })
             end, QBCore.Functions.GetPlayerData().gang.name)
-
-
             lib.showContext('gang_menu')
         end, ply.gang.name)
     end, ply.gang.name)
 end)
-function _0xhas(item)
-    return QBCore.Functions.HasItem(item)
-end
 
 RegisterNetEvent('core-gangsystem:client:updategrade', function(data)
     TriggerServerEvent('core-gangsystem:server:updategrade', data)
@@ -308,7 +279,6 @@ RegisterNetEvent('core-gangsystem:client:ManageMember', function(data)
     end
     table.insert(ManageMembers, {
         title = "Fire Member",
-
         event = 'core-gangsystem:client:firemember',
         args = {
             cid = data.player.id,
@@ -324,15 +294,10 @@ RegisterNetEvent('core-gangsystem:client:ManageMember', function(data)
     })
     lib.showContext('grade_member')
 end)
+
 RegisterNetEvent('core-gangsystem:client:firemember', function(data)
     TriggerServerEvent('core-gangsystem:server:firemember', data)
 end)
-function InviteMembers()
-    local input = lib.inputDialog('Invite a member', { 'PlayerId' })
-    local playerData = QBCore.Functions.GetPlayerData()
-    if not input then return end
-    TriggerServerEvent('core-gangsystem:serversendinvite', input[1], playerData.gang.name)
-end
 
 RegisterNetEvent('core-gangsystem:client:setgang', function(PlayerId, gangname)
     local alert = lib.alertDialog({
@@ -356,28 +321,3 @@ RegisterNetEvent('core-gangsystem:client:setgang', function(PlayerId, gangname)
         })
     end
 end)
---[[
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-https://discord.gg/k8XNJbD4T7
-
-    cccccccccccccccc        ooooooooooo        rrrrr   rrrrrrrrr            eeeeeeeeeeee
-  cc:::::::::::::::c      oo:::::::::::oo      r::::rrr:::::::::r         ee::::::::::::ee
- c:::::::::::::::::c     o:::::::::::::::o     r:::::::::::::::::r       e::::::eeeee:::::ee
-c:::::::cccccc:::::c     o:::::ooooo:::::o     rr::::::rrrrr::::::r     e::::::e     e:::::e
-c::::::c     ccccccc     o::::o     o::::o      r:::::r     r:::::r     e:::::::eeeee::::::e
-c:::::c                  o::::o     o::::o      r:::::r     rrrrrrr     e:::::::::::::::::e
-c:::::c                  o::::o     o::::o      r:::::r                 e::::::eeeeeeeeeee
-c::::::c     ccccccc     o::::o     o::::o      r:::::r                 e:::::::e
-c:::::::cccccc:::::c     o:::::ooooo:::::o      r:::::r                 e::::::::e
- c:::::::::::::::::c     o:::::::::::::::o      r:::::r                  e::::::::eeeeeeee
-  cc:::::::::::::::c      oo:::::::::::oo       r:::::r                   ee:::::::::::::e
-    cccccccccccccccc        ooooooooooo         rrrrrrr                     eeeeeeeeeeeeee
-
-
-
-]]
